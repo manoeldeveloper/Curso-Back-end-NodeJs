39bc9eb53ed8aa0e4a6c6df5b22c93a5c074512c
Manoel Faustino <contatomanoelfaustino@gmail.com>
2023-12-04 20:09:46 -0300

    Transformando em métodos e utilizando POO

--- Projects nodeJs/AtividadeArray.js
+++ Projects nodeJs/AtividadeArray.js
@@ -1,43 +1,11 @@
-import { add } from './funcoes.js';
-import { listar } from './funcoes.js';
-import { remover } from './funcoes.js';
-import { att } from './funcoes.js';
-import { consultaId } from './funcoes.js';
-import { sair } from './funcoes.js';
+import { Funcoes } from '/Users/manoelneto/Downloads/Projects nodeJs/Funcoes.js';
 
-import readlineSync from 'readline-sync';
-
-export var clientes = [];
-
-
-inicio();
-
-export function inicio() {
-    var opt = parseInt(readlineSync.question("Escolha a opção: 1 - Adicionar, 2 - Listar, 3 - Remover, 4 - Atualizar, 5 - Consultar por Id, 6 - Sair "));
-
-    switch (opt) {
-        case 1:
-            add();
-            break;
-        case 2:
-            listar();
-            break;
-        case 3:
-            remover();
-            break;
-        case 4:
-            att();
-            break;
-        case 5:
-            consultaId();
-            break;
-        case 6:
-            sair();
-            break;    
-        default:
-            console.log("Digite uma opção válida");
-            inicio();
-            break;
+class AtividadeArray {
+    inicio() {
+        const test = new Funcoes();
+        test.inicio();
     }
-    inicio();
 }
+
+const atividadeArray = new AtividadeArray();
+atividadeArray.inicio();
--- Projects nodeJs/funcoes.js
+++ Projects nodeJs/funcoes.js
@@ -1,61 +1,100 @@
-import { clientes } from "./AtividadeArray.js";
 import readlineSync from 'readline-sync';
 
-export function add() {
-    var cliente = {
-        id: clientes.length + 1, 
-        nome: readlineSync.question("Digite o nome do cliente: "),
-        email: readlineSync.question("Digite o email do cliente: ")
-    };
-
-    clientes.push(cliente);
-
-    console.log("Cliente adicionado com sucesso!");
-
-}
-
-export function listar() {
-    console.table(clientes);
-}
-
-export function remover() {
-    var idRemover = parseInt(readlineSync.question("Digite o Id do cliente: "));
-        let indexToRemove = clientes.findIndex(cliente => cliente.id === idRemover);
-             if (indexToRemove !== -1) {
-                clientes.splice(indexToRemove, 1);
-        console.log(`Cliente com ID ${idRemover} removido com sucesso.`);
-    } else {
-        console.log(`Cliente com ID ${idRemover} não encontrado.`);
+export class Funcoes {
+    constructor() {
+        this.clientes = [];
     }
-}
 
-export function att() {
-    var idAtt = parseInt(readlineSync.question("Digite o Id do cliente: "));
-        let indexToAtt = clientes.findIndex(cliente => cliente.id === idAtt);
+    inicio() {    
+        var opt = parseInt(readlineSync.question("Escolha a opção: 1 - Adicionar, 2 - Listar, 3 - Remover, 4 - Atualizar, 5 - Consultar por Id, 6 - Sair "));
+    
+        switch (opt) {
+            case 1:
+                this.add();
+                break;
+            case 2:
+                this.listar();
+                break;
+            case 3:
+                this.remover();
+                break;
+            case 4:
+                this.att();
+                break;
+            case 5:
+                this.consultaId();
+                break;
+            case 6:
+                this.sair();
+                break;    
+            default:
+                console.log("Digite uma opção válida");
+                this.inicio();
+                break;
+        }
+    }
+
+    add() {
+        var cliente = {
+            id: this.clientes.length + 1, 
+            nome: readlineSync.question("Digite o nome do cliente: "),
+            email: readlineSync.question("Digite o email do cliente: ")
+        };
+
+        this.clientes.push(cliente);
+        console.log("Cliente adicionado com sucesso!");
+        teste.inicio();
+    }
+
+    listar() {
+        console.table(this.clientes);
+        teste.inicio();
+    }
+
+    remover() {
+        var idRemover = parseInt(readlineSync.question("Digite o Id do cliente: "));
+        let indexToRemove = this.clientes.findIndex(cliente => cliente.id === idRemover);
+        if (indexToRemove !== -1) {
+            this.clientes.splice(indexToRemove, 1);
+            console.log(`Cliente com ID ${idRemover} removido com sucesso.`);
+        } else {
+            console.log(`Cliente com ID ${idRemover} não encontrado.`);
+        }
+        teste.inicio();
+    }
+
+    att() {
+        var idAtt = parseInt(readlineSync.question("Digite o Id do cliente: "));
+        let indexToAtt = this.clientes.findIndex(cliente => cliente.id === idAtt);
         if (indexToAtt !== -1) {
             let novoNome = readlineSync.question("Digite o novo nome do cliente: ");
-            clientes[indexToAtt].nome = novoNome;
+            this.clientes[indexToAtt].nome = novoNome;
             let novoEmail = readlineSync.question("Digite o novo e-mail do cliente: ");
-            clientes[indexToAtt].email = novoEmail;
+            this.clientes[indexToAtt].email = novoEmail;
     
             console.log(`Cliente com ID ${idAtt} atualizado com sucesso.`);
         } else {
             console.log(`Cliente com ID ${idAtt} não encontrado.`);
         }
-}
+        teste.inicio();
+    }
 
-export function consultaId() {
-    var idCons = parseInt(readlineSync.question("Digite o Id do cliente: "));
+    consultaId() {
+        var idCons = parseInt(readlineSync.question("Digite o Id do cliente: "));
+        let clienteEncontrado = this.clientes.find(cliente => cliente.id === idCons);
         
-            for(var i = 0; i < idCons; i++) {
-                if(idCons === clientes[i].id) {
-                    console.table(clientes[i]);
-                } else {
-                    console.log("Não existe");
-                }
-            }    
+        if (clienteEncontrado) {
+            console.table(clienteEncontrado);
+        } else {
+            console.log("Cliente não encontrado.");
+        }
+        teste.inicio();
+    }
+
+    sair() {
+        process.exit(0);
+    }
 }
 
-export function sair() {
-    process.exit(0);
-}
+const teste = new Funcoes();
+teste.inicio();
